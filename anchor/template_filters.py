c2ce83345bad58dc698edc7fc05f3ef050366847
davi4261 <david.kludt@rackspace.com>
2015-02-09 10:22:03 -0600

    Remove uneeded functions as they are not used

--- anchor/template_filters.py
+++ anchor/template_filters.py
@@ -11,34 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import re
-
-
-def nl2br(value):
-    if value:
-        _newline_re = re.compile(r'(?:\r\n|\r|\n)')
-        return _newline_re.sub('<br>', value)
-
-
-def tab2spaces(value):
-    if value:
-        text = re.sub('\t', '&nbsp;' * 4, value)
-        return text
-
-
-def unslug(value):
-    text = re.sub('_', ' ', value)
-    return text
-
-
-def slug(value):
-    text = re.sub('\s+', '_', value)
-    return text
-
-
-def check_regex(value):
-    if re.match('variable', value):
-        return True
-    else:
-        return False
