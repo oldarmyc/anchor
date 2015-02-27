df62837574319ed2647c52a53ca61ad81af1a05a
davi4261 <david.kludt@rackspace.com>
2015-02-27 15:47:30 -0600

    Add in template function to parse out metadata reboot

--- anchor/context_functions.py
+++ anchor/context_functions.py
@@ -49,9 +49,21 @@ def utility_processor():
     def get_formatted_server_list(data):
         return helper.format_server_list_for_web(data)
 
+    def process_reboot_data(data):
+        if data:
+            temp = data.split(';')
+            dates = [
+                parse(x).strftime(
+                    '%m-%d-%Y @ %r %Z'
+                ) for x in temp if x is not None
+            ]
+            return ' - '.join(dates)
+        return '-'
+
     return dict(
         unslug=unslug,
         display_date=display_date,
         get_formatted_server_list=get_formatted_server_list,
-        generate_server_age=generate_server_age
+        generate_server_age=generate_server_age,
+        process_reboot_data=process_reboot_data
     )
